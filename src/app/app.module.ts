cd482d6cda7a187f16b03582fb9a65ed78b6b4a8
lerman251 <63618979+lerman251@users.noreply.github.com>
2024-02-02 14:07:08 -0600

    done and bootstrap works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe,
+    SortPipe,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
